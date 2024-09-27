5c8b1689ef11941c178a9cc807ca679a1c9f54e3
Marley <Marley@ENROXES>
2024-09-27 22:17:03 +0500

    Добавьте файлы проекта.

---
+++ pr2_c++_zad2.cpp
@@ -0,0 +1,32 @@
+﻿using namespace std;
+
+#include <iostream>
+
+int main()
+{
+    setlocale(0, "rus");
+    const int row = 4;
+	const int col = 6;
+    int M[row][col];
+    cout << "Двумерный массив заполненный случайными числами: \n";
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+			M[i][j] = rand() % 20;
+			cout << M[i][j] << " ";
+		}
+		cout << endl;
+    }
+    int sum;
+    for (int i = 0; i < row; i++) 
+    {
+        sum = 0;
+        for (int j = 0; j < col; j++) 
+        {
+            sum += M[i][j];
+        }
+        cout << "Сумма строки " << i << ": " << sum << endl;
+    }
+}
+
